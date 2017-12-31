85533c2326db657a329b4bad13fc763def5c0b77
dargavavid <dargavavid@gmail.com>
2017-12-31 21:22:57 +0100

    Add function to handle ball boundaries

--- js/main.js
+++ js/main.js
@@ -11,3 +11,18 @@ var paddle = new Entity(75, $fg.height - 10, 75, 10);
 function updateBallLocation() {
     ball.updateLocation.call(ball);
 }
+
+function handleBoundaries() {
+    if (ball.x < 0) {
+        ball.x = 0; //Reset ball's x coordinate inside of boundary.
+        ball.invertDirection(-1, 1); //Bounce the ball back.
+    }
+    if (ball.x + ball.width > $fg.width) {
+        ball.x = $fg.width - ball.width; //Reset ball's x coordinate inside of boundary.
+        ball.invertDirection(-1, 1);
+    }
+    if (ball.y < 0) {
+        ball.y = 0; //Reset ball's y coordinate inside of boundary.
+        ball.invertDirection(1, -1);
+    }
+}
